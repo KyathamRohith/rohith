896858aca8a4e858851d38385a50a350f69151f6
KyathamRohith <kyathamrohith76@gmail.com>
2025-01-06 15:00:39 +0530

    add

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  
